cf1550f7f58b5e3ca9443d68b8cce28c5c1327d4
usernamen <booglemoogle23@gmail.com>
2021-06-06 17:24:41 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+   iconMenu.addEventListener("click", function (e) {
+       iconMenu.classList.toggle('_active');
+       menuBody.classList.toggle('_active');
+  });
+
+
+
+
+
+
+
